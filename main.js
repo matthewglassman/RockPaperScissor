432735c4dbfbd12fcfb2094f5e9fb51c6134d99f
Matthew Glassman <matthewglassman78@gmail.com>
2022-02-25 15:51:16 -0500

    Altered If statement to user variable userInput in each OR statement.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function getUserChoice (userInput){
     userInput = userInput.toLowerCase();
 
-    if (userInput === 'rock' || 'paper' || 'scissors'){
+    if (userInput === 'rock' || userInput === 'paper' || userInput ==='scissors'){
         return userInput;
     }else{
         console.log('Please enter a valid choice rock, paper or scissor.');
