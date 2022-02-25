d7035036c6d7fa29ebdbfa84a0a6eac888d9c486
Matthew Glassman <matthewglassman78@gmail.com>
2022-02-25 14:15:31 -0500

    Initial Commit. Create function to take in user RPS choice.

---
+++ main.js
@@ -0,0 +1,9 @@
+function getUserChoice (userInput){
+    userInput = userInput.toLowerCase();
+
+    if (userInput === 'rock' || 'paper' || 'scissors'){
+        return userInput;
+    }else{
+        console.log('Please enter a valid choice rock, paper or scissor.');
+    }
+}
