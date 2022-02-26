b9c81899c99b005178dbe8f06442f78f3aa43a69
Matthew Glassman <matthewglassman78@gmail.com>
2022-02-25 20:25:39 -0500

    Removing console.log from Computer Choice function.

--- main.js
+++ main.js
@@ -15,13 +15,10 @@ function getComputerChoice () {
     console.log(computerChoice);
 
     if (computerChoice === 0) {
-        console.log('rock');
         return 'rock';
     } else if (computerChoice === 1) {
-        console.log('paper');
         return 'paper';
     } else {
-        console.log('scissors');
         return 'scissors';
     }
 }
