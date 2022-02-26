5446ddd796938ca437657f58a6e0ac046784e625
Matthew Glassman <matthewglassman78@gmail.com>
2022-02-25 20:24:37 -0500

    Testing computer choices with console logs and if/else conditionals.

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
-function getUserChoice (userInput){
-    userInput = userInput.toLowerCase();
+// function getUserChoice (userInput){
+//     userInput = userInput.toLowerCase();
 
-    if (userInput === 'rock' || userInput === 'paper' || userInput ==='scissors'){
-        return userInput;
-    }else{
-        console.log('Please enter a valid choice rock, paper or scissor.');
+//     if (userInput === 'rock' || userInput === 'paper' || userInput ==='scissors'){
+//         return userInput;
+//     }else{
+//         console.log('Please enter a valid choice rock, paper or scissor.');
+//     }
+// }
+
+//getUserChoice('ROCK');
+
+function getComputerChoice () {
+    computerChoice = Math.floor(Math.random() * 3);
+    console.log(computerChoice);
+
+    if (computerChoice === 0) {
+        console.log('rock');
+        return 'rock';
+    } else if (computerChoice === 1) {
+        console.log('paper');
+        return 'paper';
+    } else {
+        console.log('scissors');
+        return 'scissors';
     }
 }
 
-getUserChoice('ROCK');
+getComputerChoice();
